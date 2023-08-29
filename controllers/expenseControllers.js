5b30d260cd6f220926e6c4637b27a283cb60d04a
asitpal211 <asitpal211@gmail.com>
2023-08-29 16:07:10 +0530

    added sign up backend api

--- controllers/expenseControllers.js
+++ controllers/expenseControllers.js
@@ -1,6 +1,16 @@
-// const  user = require('../models/user');
+const  user = require('../models/user');
 
-// exports.addUser = async(req, res, next) => {
-//     try{}
+exports.addUser = async(req, res, next) => {
+    try {
+        
+        const { name, email, password} = req.body;
+        const newUser = await user.create({name, email, password});
+        res.status(200).json({newUser});
+  
+    } catch (error) {
+        
+        console.log(error);
+        res.status(500).json({error: 'Internal Server error'});
+    }
 
-// };
+};
--- public/main.js
+++ public/main.js
@@ -1,13 +1,32 @@
+
 document.addEventListener("DOMContentLoaded", function(){
 
     const signupButton = document.getElementById('button');
     signupButton.addEventListener("click", function() {
 
-        const name = document.getElementById('name');
-        const email = document.getElementById('email');
-        const password = document.getElementById('password');
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
         
+
+        const user = {
+            name:name,
+            email:email,
+            password:password
+        };
+        console.log(user);
+
+        axios.post(`http://localhost:3000/user/signup`, user)
+        .then(response => {
+            console.log(response);
+        })
+        .catch(err => {
+            console.log(err);
+
+        });
+
+
 
     });
 });
