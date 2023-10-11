0f049d07d658685d15af0128a29e6de7b2f07b37
asitpal211 <asitpal211@gmail.com>
2023-10-11 12:41:20 +0530

    final commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+require('dotenv').config(); 
 const express = require("express");
+const fs = require('fs');
 const path = require("path");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 const sequelize = require("./util/database");
 const expenseRoutes = require("./routes/expenseRoutes");
@@ -11,11 +12,10 @@ const forgotPasswordRequest = require("./models/forgotPasswordRequest");
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-
+app.use(express.json());  
 app.use(express.static("public"));
 
-app.get("/login", (req, res) => {
+app.get("/login", (req, res) => { 
   res.sendFile(__dirname + "/views/login.html");
 });
 
@@ -54,7 +54,7 @@ app.use("/", expenseRoutes);
 sequelize
   .sync()
   .then(() => {
-    app.listen(3000, () => {
+    app.listen(process.env.PORT || 3000, () => {
       console.log(`Server is running on port ${3000}`);
     });
   })
--- public/main.js
+++ public/main.js
@@ -8,9 +8,9 @@ document.addEventListener("DOMContentLoaded", function () {
     signupButton.addEventListener("click", async function (event) {
       event.preventDefault();
       const name = document.getElementById("name").value;
-      const email = document.getElementById("email").value;
+      const email = document.getElementById("email").value; 
       const password = document.getElementById("password").value;
-
+ 
       const user = {
         name: name,
         email: email,
